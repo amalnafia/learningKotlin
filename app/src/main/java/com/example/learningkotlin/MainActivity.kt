199271b59cf07d1b6f0fe31630aec1ab2de1d263
FastTrack <FastTrack@DESKTOP-2SN9M6U>
2020-03-09 17:37:25 +0200

    Coding with mitch kotlin tutorial for beginners

---
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.learningkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var blogAdapter: BlogRecyclerAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet() {
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView() {
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
+//        recycler_view.layoutManager = LinearLayoutManager(this@MainActivity)
+//        blogAdapter = BlogRecyclerAdapter()
+//        recycler_view.adapter = blogAdapter
+    }
+}
